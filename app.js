d1034bfc175ffe81f22d44704004c6ccf9cf02f9
Helen Yu <helenyueyu@gmail.com>
2019-07-13 16:44:48 -0700

    Second commit

--- app.js
+++ app.js
@@ -1,11 +1,42 @@
 const express = require('express'); 
 const app = express(); 
+const db = require('./config/keys').mongoURI;
+const mongoose = require('mongoose'); 
+const bodyParser = require('body-parser'); 
+const passport = require('passport'); 
+
+const users = require('./routes/api/users'); 
+const tweets = require('./routes/api/tweets'); 
+
+const User = require('./models/User'); 
+
+
+mongoose.connect(db, { useNewUrlParser: true })
+    .then(() => console.log('Connected to mongoDB'))
+    .catch(err => console.log(err)); 
+
+app.use(passport.initialize()); 
+require('./config/passport')(passport); 
+
+app.use(bodyParser.urlencoded({
+    extended: false 
+})); 
+app.use(bodyParser.json()); 
 
 app.get("/", (req, res) => {
+    const user = new User({
+        handle: "jim", 
+        email: "jim@jim.jim", 
+        password: "jimisgreat123"
+    })
+    user.save(); 
     res.send("Hello Wd!"); 
 }); 
 
-const port = process.env.PORT || 3000; 
+app.use("/api/users", users); 
+app.use("/api/tweets", tweets); 
+
+const port = process.env.PORT || 5000; 
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
