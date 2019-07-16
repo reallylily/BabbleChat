481489c51623f50db27d8ef1d71e2b428d05422f
Helen Yu <helenyueyu@gmail.com>
2019-07-16 16:33:05 -0700

    Add removing bubble functionality

--- frontend/src/components/translate/translate_word.jsx
+++ frontend/src/components/translate/translate_word.jsx
@@ -10,27 +10,38 @@ class TranslateWord extends React.Component {
         // this.state = this.props
         this.handleClick = this.handleClick.bind(this)
         this.translate = setCORS("https://pure-headland-71945.herokuapp.com/");
-        this.bubble = null
-        
+        this.state = {
+            bubble: null
+        } 
+        // this.bubble = null
     }
     handleClick() {
+        if (this.state.bubble === null) {
+            console.log('button clicked')
+            let text = this.props.text
+            // let to_learn = this.props.user.to_learn
+            let to_share = this.props.user.to_share
+            this.translate(text, { to: to_share })
+                .then(res => {
+                    this.setState({ bubble: this.state.bubble = () => (<Bubble text={res.text} response={res} />) })
+                    console.log(res.text)
+                    // console.log(res)
+                    // console.log(this)
+                })
+                .catch(err => {
+                    console.error(err);
+                });
+        } else {
+            console.log('hitting this')
+            console.log(this)
+            this.setState({
+                bubble: null
+            })
+        }
         // e.preventDefault();
         // this.translate = setCORS("http://cors-anywhere.herokuapp.com/");
         // console.log(this)
-        console.log('button clicked')
-        let text = this.props.text
-        // let to_learn = this.props.user.to_learn
-        let to_share = this.props.user.to_share
-        this.translate(text, { to: to_share })
-          .then(res => {
-              this.setState({ bubble : this.bubble = ()=>(<Bubble text={res.text} response={res}/> ) })
-            console.log(res.text)
-            // console.log(res)
-            // console.log(this)
-          })
-          .catch(err => {
-            console.error(err);
-          });
+
     }
 
     render() {
@@ -59,7 +70,7 @@ class TranslateWord extends React.Component {
         }
         return (
             <div style={{ display: 'flex', flexDirection: 'column'}}>
-                <span className="chat-box-bubble">{this.bubble ? this.bubble() : null }</span>
+                <span className="chat-box-bubble">{this.state.bubble ? this.state.bubble() : null }</span>
                 {button()}
                 
                 {/* {this.props.myIndex === 0 || this.props.myIndex == this.props.myLength - 1 ? 
